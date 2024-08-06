8aa444bd0240259b44eaf570f6fe1b3fe32fdb03
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2024-08-07 00:06:43 +0200

    only attract on zero-g

--- src/attract.js
+++ src/attract.js
@@ -4,18 +4,22 @@ import { listen } from './dispatch.js'
 export const addAttractionOnHold = (engine) => {
   listen('hold', pos => {
     const boxes = Matter.Composite.allBodies(engine.world).filter(b => b.kind === 'box')
-    boxes.forEach(box => {
-      const distance = Matter.Vector.magnitude(
-        Matter.Vector.sub(pos, box.position)
-      )
-  
-      const f = Matter.Vector.mult(
-        Matter.Vector.normalise(
+    if (engine.gravity.scale === 0) {
+      boxes.forEach(box => {
+        const distance = Matter.Vector.magnitude(
           Matter.Vector.sub(pos, box.position)
-        ), 0.005
-      )
-
-      Matter.Body.applyForce(box, pos, f)
-    })
+        )
+    
+        if (distance < BOX_CONFIG.SIZE * 5) {
+          const f = Matter.Vector.mult(
+            Matter.Vector.normalise(
+              Matter.Vector.sub(pos, box.position)
+            ), 0.005
+          )
+  
+          Matter.Body.applyForce(box, pos, f)
+        }
+      })
+    }
   })
 }
