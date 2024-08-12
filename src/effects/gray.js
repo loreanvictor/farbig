ee4a48a1651afd7b076630a7415abf5d948d95e6
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2024-08-12 19:20:45 +0300

    rework gray special

--- src/effects/gray.js
+++ src/effects/gray.js
@@ -1,14 +1,16 @@
 import { listen } from '../dispatch.js'
 import { CHOSEN_COLOR } from '../score.js'
-import { GRAY, BOX_CONFIG } from '../box/index.js'
+import { GRAY } from '../box/index.js'
 
 
 export const addGrayEffect = (engine) => {
+  const indicator = document.getElementById('antigrav')
+
   let antiGravCounter = 0
   let gravRunner
   const maxGravCounter = 12
   const DEFAULT_GRAVITY = 0.001
-  let attractColors = []
+  let attractColor = undefined
   
   const turnGravityOff = (mul = 1) => {
     const mult = CHOSEN_COLOR === GRAY ? 2 : 1
@@ -20,10 +22,11 @@ export const addGrayEffect = (engine) => {
         if (antiGravCounter > 0) {
           engine.gravity.scale = 0
           antiGravCounter--
-          document.getElementById('antigrav').style.transform = `scaleX(${antiGravCounter / (maxGravCounter * mult)})`
+          indicator.style.transform = `scaleX(${antiGravCounter / (maxGravCounter * mult)})`
         } else {
           engine.gravity.scale = DEFAULT_GRAVITY
-          attractColors = []
+          attractColor = undefined
+          indicator.style.borderColor = 'transparent'
         }
       }, 300)
     }
@@ -32,16 +35,9 @@ export const addGrayEffect = (engine) => {
   listen('pop:box', ({ box, group }) => {
     if (box.tag === GRAY) {
       turnGravityOff(group.length)
-      const boxes = Matter.Composite.allBodies(engine.world).filter(b => b.kind === 'box')
-
-      boxes.forEach(b => {
-        if (b.tag !== GRAY && !attractColors.includes(b.tag)) {
-          const dist = Matter.Vector.magnitude(Matter.Vector.sub(b.position, box.position))
-          if (dist < BOX_CONFIG.SIZE * 1.2) {
-            attractColors.push(b.tag)
-          }
-        }
-      })
+    } else if (engine.gravity.scale === 0) {
+      attractColor = box.tag
+      indicator.style.borderColor = attractColor
     }
   })
 
@@ -49,11 +45,11 @@ export const addGrayEffect = (engine) => {
     const boxes = Matter.Composite.allBodies(engine.world).filter(b => b.kind === 'box')
     if (engine.gravity.scale === 0) {
       boxes.forEach(box => {
-        if (attractColors.includes(box.tag)) {
+        if (box.tag === attractColor) {
           const f = Matter.Vector.mult(
             Matter.Vector.normalise(
               Matter.Vector.sub(pos, box.position)
-            ), 0.01
+            ), 0.05
           )
   
           Matter.Body.applyForce(box, pos, f)
