fbeeef56bc01500f25c0c12ee4f0765656c16024
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2024-08-27 22:52:30 +0200

    make white favor absent colors from affected boxes

--- src/effects/white.js
+++ src/effects/white.js
@@ -12,6 +12,7 @@ export const addWhiteEffect = (engine) => {
       const mult = CHOSEN_COLOR === WHITE ? 2 : 1
       const gmult = Math.max(Math.floor(Math.log(group.length * 1.7) / Math.log(2)), 1)
 
+      const toBeChanged = []
       boxes.forEach(box => {
         const distance = group.reduce(
           (m, b) => Math.min(m, Matter.Vector.magnitude(Matter.Vector.sub(box.position, b.position))),
@@ -22,10 +23,21 @@ export const addWhiteEffect = (engine) => {
           !boxes.some(b => b.tag === box.tag && b.isStatic) &&
           distance < BOX_CONFIG.SIZE * gmult * 1.5 * mult
         ) {
+          toBeChanged.push(box)
+        }
+      })
+
+      toBeChanged.forEach(box => {
           const colors = all.filter(c => c !== box.tag)
+          all.forEach(color => {
+            if (!toBeChanged.some(b => b.tag === color)) {
+              colors.push(color)
+              colors.push(color)
+            }
+          })
+
           const color = colors[random(0, colors.length - 1)]
           changeColor(box, color)
-        }
       })
     }
   })
