687f21e4e18e26419c71468a2a844a0f0c2bf90a
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2024-12-07 01:46:56 +0100

    rescale spin bonus

--- src/effects/speed.js
+++ src/effects/speed.js
@@ -3,9 +3,9 @@ import { addScore } from '../score.js'
 
 
 export const addSpeedBonus = () => {
-  const HIGH_ROTATE_BONUS = 420_000
+  const HIGH_ROTATE_BONUS = 240_000
   const MIN_ANG_SPEED = 0.1
-  const EXP_ANG_SPEED = 0.4
+  const EXP_ANG_SPEED = 0.8
 
   listen('box:popped', ({ box }) => {
     if (box.angularSpeed > MIN_ANG_SPEED) {
